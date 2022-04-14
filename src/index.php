4103f225f0df691195c1618fdab2c2a57f5f2b25
Kenta Kawase <kawase-k@spc-jpn.co.jp>
2022-04-14 20:49:06 +0900

    Add:top途中まで追加

--- src/index.php
+++ src/index.php
@@ -31,19 +31,170 @@
                         <div class="topIntroAboutus__link--item noto">
                             <span class="num lato">#3</span>
                             SDGsへの貢献
-                            <img src="<?php echo T_URL ?>img/arro w.svg" alt="" class="icon">
+                            <img src="<?php echo T_URL ?>img/arrow.svg" alt="" class="icon">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="topIntroBusiness">
+
+        <div class="topBusiness">
             <div class="wrap w1200 sp-wrap">
                 <h2 class="top-title">
                     事業紹介
                 </h2>
             </div>
         </div>
+        <div class="topBusinessLink flex bet">
+            <a href="" class="topBusinessLink__item">
+                <img src="<?php echo T_URL ?>img/topbusiness_link1.png" alt="">
+            </a>
+            <a href="" class="topBusinessLink__item">
+                <img src="<?php echo T_URL ?>img/topbusiness_link2.png" alt="">
+            </a>
+            <a href="" class="topBusinessLink__item">
+                <img src="<?php echo T_URL ?>img/topbusiness_link3.png" alt="">
+            </a>
+        </div>
+        <div class="topBusiness__copy">
+            <div class="wrap w1200 sp-wrap">
+                <div class="topBusiness__copy--ttl">
+                    シナネンHDグループが描く未来
+                </div>
+                <p class="topBusiness__copy--para">
+                    概要が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                </p>
+            </div>
+        </div>
     </div>
+
+
+    <div class="topInterview">
+        <div class="topInterview__bg"></div>
+        <div class="wrap w1200 sp-wrap">
+            <h2 class="top-title flex vcenter">
+                インタビュー<span class="top-title--small">～わたしたちの"挑戦"～</span>
+            </h2>
+            <div class="topInterviewWrap flex bet">
+                <div class="topInterviewItem">
+                    <div class="topInterviewItem__img">
+                        <img src="<?php echo T_URL ?>img/top_interview1.png" alt="">
+                    </div>
+                    <div class="topInterviewItem__text">
+                        <div class="topInterviewItem__text--ttl">
+                            キャッチコピー（仮）テキストテキストテキストテキストテキスト
+                        </div>
+                        <div class="topInterviewItem__text--positon">
+                            部署（所属）<br>
+                            <span class="join">0000年入社　●● ●●</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topInterviewItem">
+                    <div class="topInterviewItem__img">
+                        <img src="<?php echo T_URL ?>img/top_interview2.png" alt="">
+                    </div>
+                    <div class="topInterviewItem__text">
+                        <div class="topInterviewItem__text--ttl">
+                            キャッチコピー（仮）テキストテキストテキストテキストテキスト
+                        </div>
+                        <div class="topInterviewItem__text--positon">
+                            部署（所属）<br>
+                            <span class="join">0000年入社　●● ●●</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topInterviewItem">
+                    <div class="topInterviewItem__img">
+                        <img src="<?php echo T_URL ?>img/top_interview1.png" alt="">
+                    </div>
+                    <div class="topInterviewItem__text">
+                        <div class="topInterviewItem__text--ttl">
+                            キャッチコピー（仮）テキストテキストテキストテキストテキスト
+                        </div>
+                        <div class="topInterviewItem__text--positon">
+                            部署（所属）<br>
+                            <span class="join">0000年入社　●● ●●</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="topInterviewItem">
+                    <div class="topInterviewItem__img">
+                        <img src="<?php echo T_URL ?>img/top_interview2.png" alt="">
+                    </div>
+                    <div class="topInterviewItem__text">
+                        <div class="topInterviewItem__text--ttl">
+                            キャッチコピー（仮）テキストテキストテキストテキストテキスト
+                        </div>
+                        <div class="topInterviewItem__text--positon">
+                            部署（所属）<br>
+                            <span class="join">0000年入社　●● ●●</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="topCrosstalk">
+        <div class="topCrosstalk__bg"></div>
+        <div class="wrap w1200 sp-wrap">
+            <h2 class="top-title flex vcenter">
+                クロストーク
+            </h2>
+        </div>
+        <div class="topCrosstalkSlide swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide topCrosstalkSlide__item">
+                    <span class="topCrosstalkSlide__item--caption">
+                        <span class="bg">Special Crosstalk～ 女性のキャリアアップ ～</span>
+                    </span>
+                    <img src="<?php echo T_URL ?>img/crosstalk1.png" alt="">
+                </div>
+                <div class="swiper-slide topCrosstalkSlide__item">
+                    <span class="topCrosstalkSlide__item--caption">
+                        <span class="bg">Special Crosstalk～ 女性のキャリアアップ ～</span>
+                    </span>
+                    <img src="<?php echo T_URL ?>img/crosstalk1.png" alt="">
+                </div>
+                <div class="swiper-slide topCrosstalkSlide__item">
+                    <span class="topCrosstalkSlide__item--caption">
+                        <span class="bg">Special Crosstalk～ 女性のキャリアアップ ～</span>
+                    </span>
+                    <img src="<?php echo T_URL ?>img/crosstalk1.png" alt="">
+                </div>
+            </div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+        <!-- Swiper END -->
+    </div>
+
+
+    <div class="topRecruit">
+        <div class="topRecruit__bg"></div>
+        <div class="wrap w1200 sp-wrap">
+            <h2 class="top-title flex vcenter">
+                採用情報
+            </h2>
+        </div>
+        <div class="wrap w1200 sp-wrap">
+            <div class="topRecruitWrap flex bet">
+                <?php $arr = array('募集要項', '求める人物像', '採用フロー', '働く環境', 'よくある質問');
+                $num = 0;
+                foreach ($arr as $text) {
+                    $num++;
+                ?>
+                    <div class="topRecruit__item">
+                        <img src="<?php echo T_URL ?>img/topRecruit<?php echo $num ?>.svg" alt="" class="topRecruit__item--img">
+                        <p class="topRecruit__item--ttl">
+                            <?php echo $text ?>
+                        </p>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
 </article>
 <?php get_footer();
