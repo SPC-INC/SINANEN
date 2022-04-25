1b7f8759d330469b75c3ce0fd784b1b7f7af8d6b
DESKTOP-D0KA0HO\SPC-37 <morimoto-c@spc-jpn.co.jp>
2022-04-25 20:42:41 +0900

    Add: BtoB、BtoC

---
+++ src/business/btoc/index.php
@@ -0,0 +1,126 @@
+<?php
+$include = '../../include/'; // パーツ用パス
+$home = '../../'; // サイトルートパス
+$page = array( // ページデータ
+    'flag' => 'btoc', // ページ判別フラグ
+    'slug' => '', // TOP以降のURL 末尾の/や拡張子まで含める
+);
+require_once $include . '_header.php';
+?>
+<article class="contents contents--<?php echo IS_PAGE; ?>">
+
+    <div class="businessChildHead notos">
+        <div class="businessChildHead__img">
+            <img src="<?php echo HOME; ?>img/business_btoc_head.png" alt="" class="pc">
+            <img src="<?php echo HOME; ?>img/business_btoc_head_sp.png" alt="" class="sp">
+        </div>
+        <h1 class="businessChildHead__text flex vcenter">
+            BtoC事業<br class="sp">(エネルギー卸・小売周辺事業)
+        </h1>
+    </div>
+
+    <section class="businessChild">
+        <div class="wrap w1200 sp-wrap">
+            <p class="businessChild__intro">
+                石油・ガス・電力と、多彩なエネルギーを取り扱うシナネンHDグループでは、各部門が連携し、お客様のニーズに即したサービスとソリューションをご提案するほか、再生可能エネルギー事業、新規事業の創出などにも積極的に取り組んでいます。
+            </p>
+        </div>
+
+        <div class="businessChild__container">
+            <div class="wrap w1200 sp-wrap">
+                <div class="businessChild__wrapper pc-flex bet">
+                    <div class="pc businessChildSide">
+                        <h2 class="businessChildSide__head">
+                            エネルギーの事業
+                        </h2>
+                        <ul class="businessChildSide__list">
+                            <li class="businessChildSide__item">
+                                住まいの事業
+                            </li>
+                            <li class="businessChildSide__item">
+                                エネルギーの事業
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="businessChildMain">
+                        <div class="businessChildMain__item">
+                            <h2 class="businessChildMain__head notos js-in anime left-in">
+                                エネルギーの事業
+                            </h2>
+                            <p class="businessChildMain__img js-in anime bottom-in">
+                                <img src="<?php echo HOME; ?>img/business_btoc_01.png" alt="">
+                            </p>
+                            <p class="businessChildMain__text js-in anime bottom-in">
+                                事業内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                            </p>
+                            <div class="businessChildMain__job js-in anime bottom-in">
+                                <h3 class="businessChildMain__job--head">
+                                    仕事内容
+                                </h3>
+                                <p class="businessChildMain__job--text">
+                                    具体的な仕事内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                </p>
+                            </div>
+                        </div>
+                        <div class="businessChildMain__item">
+                            <h2 class="businessChildMain__head notos js-in anime left-in">
+                                住まいの事業
+                            </h2>
+                            <p class="businessChildMain__img js-in anime bottom-in">
+                                <img src="<?php echo HOME; ?>img/business_btoc_02.png" alt="">
+                            </p>
+                            <p class="businessChildMain__text js-in anime bottom-in">
+                                事業内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                            </p>
+                            <div class="businessChildMain__job js-in anime bottom-in">
+                                <h3 class="businessChildMain__job--head">
+                                    仕事内容
+                                </h3>
+                                <p class="businessChildMain__job--text">
+                                    具体的な仕事内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                </p>
+                            </div>
+                        </div>
+                        <div class="businessChildMain__item">
+                            <h2 class="businessChildMain__head notos js-in anime left-in">
+                                暮らしの事業
+                            </h2>
+                            <p class="businessChildMain__img js-in anime bottom-in">
+                                <img src="<?php echo HOME; ?>img/business_btoc_03.png" alt="">
+                            </p>
+                            <p class="businessChildMain__text js-in anime bottom-in">
+                                事業内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                            </p>
+                            <div class="businessChildMain__job js-in anime bottom-in">
+                                <h3 class="businessChildMain__job--head">
+                                    仕事内容
+                                </h3>
+                                <p class="businessChildMain__job--text">
+                                    具体的な仕事内容が入りますテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                                </p>
+                            </div>
+                        </div>
+                        <div class="businessChildMain__link js-in anime bottom-in">
+                            <a href="<?php echo HOME; ?>business/" class="businessChildMain__btn md-btn red">
+                                事業一覧へ
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 71.493 6.909" class="icon">
+                                    <g id="コンポーネント_13_1" data-name="コンポーネント 13 – 1" transform="translate(0.5 0.683)">
+                                        <line id="線_11" data-name="線 11" x2="70.309" transform="translate(0 5.542)" fill="none" stroke-linecap="round" stroke-width="1" />
+                                        <line id="線_12" data-name="線 12" x2="9.573" y2="5.543" transform="translate(60.736)" fill="none" stroke-linecap="round" stroke-width="1" />
+                                    </g>
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="businessChildMain__entry">
+                            <?php require_once $include . '_entry.php'; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</article>
+<?php
+require_once $include . '_footer.php';
