eb15dd3ce17a738e5baa00648bb96dc0d1bcbc74
DESKTOP-D0KA0HO\SPC-37 <morimoto-c@spc-jpn.co.jp>
2022-05-09 20:13:47 +0900

    Add: シナネンが描く未来

---
+++ src/future/index.php
@@ -0,0 +1,165 @@
+<?php
+$include = '../include/'; // パーツ用パス
+$home = '../'; // サイトルートパス
+$page = array( // ページデータ
+    'flag' => 'future', // ページ判別フラグ
+    'slug' => '', // TOP以降のURL 末尾の/や拡張子まで含める
+);
+require_once $include . '_header.php';
+?>
+<article class="contents contents--<?php echo IS_PAGE; ?>">
+
+    <div class="futureHead notos">
+        <div class="futureHead__img">
+            <img src="<?php echo HOME; ?>img/future_head.png" alt="" class="pc">
+            <img src="<?php echo HOME; ?>img/future_head_sp.png" alt="" class="sp">
+        </div>
+        <div class="futureHead__text">
+            <h1 class="futureHead__text--main js-in anime bottom-inn">
+                <span class="futureHead__text--en">FUTURE</span>
+                <span class="futureHead__text--ja">シナネン<span class="pc">HDグループ</span>が描く未来</span>
+            </h1>
+            <p class="futureHead__text--sub js-in anime bottom-in">
+                Challenging New Worlds with Big Sky-thinking<br>
+                ～大胆な発想で新しい世界への挑戦～
+            </p>
+            <p class="futureHead__text--sub js-in anime bottom-in">
+                私たちは「こんな未来だったらいいな」を<br class="sp">実現していきます
+            </p>
+            <p class="sp futureHead__text--sub js-in anime bottom-in">
+                scroll
+            </p>
+        </div>
+    </div>
+
+    <div class="wrap w1200 sp-wrap">
+        <section class="futureSec">
+            <h2 class="futureSec__head notos js-in anime left-in">
+                再生可能エネルギーだけで回る世界
+                <div class="futureSec__head--bar pc-flex vcenter">
+                    <span class="inner">Lifestyle</span>
+                </div>
+            </h2>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        グループの総合力で住まいと暮らしに貢献
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_lifestyle_01.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_lifestyle_01.png" alt="">
+                </div>
+            </div>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        脱炭素社会の実現
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_lifestyle_02.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_lifestyle_02.png" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="futureSec">
+            <h2 class="futureSec__head notos js-in anime left-in">
+                誰もが働きやすく生きやすく
+                <div class="futureSec__head--bar pc-flex vcenter">
+                    <span class="inner">Workstyle</span>
+                </div>
+            </h2>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        ダイバーシティ＆インクルージョン
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_workstyle_01.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_workstyle_01.png" alt="">
+                </div>
+            </div>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        風土改革
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_workstyle_02.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_workstyle_02.png" alt="">
+                </div>
+            </div>
+        </section>
+
+        <section class="futureSec">
+            <h2 class="futureSec__head notos js-in anime left-in">
+                理想とする未来のシナネンHDグループの在り方が入ります
+                <div class="futureSec__head--bar pc-flex vcenter">
+                    <span class="inner">Company</span>
+                </div>
+            </h2>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        持続的な成長
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_company_01.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_company_01.png" alt="">
+                </div>
+            </div>
+            <div class="futureSec__container pc-flex bet js-in anime bottom-in">
+                <div class="futureSec__text flex vert">
+                    <h3 class="futureSec__subhead js-in anime left-in">
+                        新規事業の創出
+                    </h3>
+                    <div class="sp futureSec__img js-in anime left-in">
+                        <img src="<?php echo HOME; ?>img/future_company_01.png" alt="">
+                    </div>
+                    <p class="futureSec__desc">
+                        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+                    </p>
+                </div>
+                <div class="pc futureSec__img js-in anime left-in">
+                    <img src="<?php echo HOME; ?>img/future_company_02.png" alt="">
+                </div>
+            </div>
+        </section>
+        <div class="futureSec__entry">
+            <?php require_once $include . '_entry.php'; ?>
+        </div>
+    </div>
+
+</article>
+<?php
+require_once $include . '_footer.php';
